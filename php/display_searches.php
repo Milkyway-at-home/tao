75af687fd8b92977d6bf890e70b9028c111aa7f5
Kevnroux <kevroux3@utexas.edu>
2024-03-06 06:48:56 -0500

    Removing debuggin lines

--- php/display_searches.php
+++ php/display_searches.php
@@ -58,9 +58,6 @@ echo "
 echo "<body>";
 echo "<form action='display_plots.php' method='get'>\n";
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 echo "
 <div class='container'>
